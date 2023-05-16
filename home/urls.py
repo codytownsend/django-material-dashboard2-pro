608e641b602614caf843e4f43c3270c49d7cb353
codytownsend <cody@zerohourlabs.com>
2023-05-16 15:20:05 -0500

    update

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    # Pages -> Jobs
+    path('jobs/', views.discover, name="jobs"),
+    path('jobs/new-user/', views.new_job, name="new_job"),
+
 ]
--- home/views.py
+++ home/views.py
@@ -7,3 +7,18 @@ def index(request):
 
     # Page from the theme 
     return render(request, 'pages/index.html')
+
+def discover(request):
+  context = {
+      'parent': 'jobs',
+      'segment': 'jobs'
+  }
+  return render(request, 'pages/pages/jobs/jobs.html', context)
+
+
+def new_job(request):
+  context = {
+      'parent': 'jobs',
+      'segment': 'new_job'
+  }
+  return render(request, 'pages/pages/jobs/new-job.html', context)
